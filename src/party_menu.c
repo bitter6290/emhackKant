50a5b9bb70d8fdb152ac22276d9356ba98c32a9b
bitter6290 <cooksons413@gmail.com>
2022-06-12 21:02:18 -0400

    Fixed party menu problem

--- src/party_menu.c
+++ src/party_menu.c
@@ -2209,7 +2209,7 @@ static void DrawEmptySlot(u8 windowId)
     if (gPartyMenu.layout == PARTY_LAYOUT_SINGLE) //Custom party menu
         BlitBitmapToPartyWindow(windowId, sEqualEmptySlotTileNums, 14, 0, 0, 14, 5);//
     else
-        BlitBitmapToPartyWindow(windowId, sEmptySlotTileNums, 18, 0, 0, 18, 3);
+        BlitBitmapToPartyWindow(windowId, sEqualEmptySlotTileNums, 18, 0, 0, 18, 3);
 }
 
 //Custom party menu
